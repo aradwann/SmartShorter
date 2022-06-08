229ddbf2753dc38bc50e44571350e59eb3faad63
Ahmed Radwan <aradwann@proton.me>
2022-06-08 11:47:18 +0200

    connect to db and register shortlinks router

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,28 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import shortLinkRouter from './routes/shortLink.routes'
+
+const url = 'mongodb://localhost:27017/smartShorter'
+const connect = mongoose.connect(url)
 
 const app = express()
 app.use(cors())
 
-app.use(bodyParser.json())
-const port = process.env.PORT || 4000
+app.use(express.json({
+  type: () => true
+}))
+const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Listening at https://localhost:${port}`)
-})
+app.use('/shortlinks', shortLinkRouter)
+
+connect
+  .then((db) => {
+    console.log('connected successfully to the database')
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
